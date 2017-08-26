b870d831d025a4030a75442a54a6ce922592cc0f
Jan-Willem Patrick van Heeswijck <jvheeswijck@users.noreply.github.com>
2017-08-26 23:24:59 +0800

    provided code for all the functions

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -2,20 +2,22 @@
 # Licensed under the Apache License, Version 2.0
 
 
-def match_ends(words):
+def match_ends(s):
     """
     Given a list of strings, return the count of the number of strings
     where the string length is 2 or more and the first and last chars
     of the string are the same.
-
-    >>> match_ends(['aba', 'xyz', 'aa', 'x', 'bbb'])
-    3
-    >>> match_ends(['', 'x', 'xy', 'xyx', 'xx'])
-    2
-    >>> match_ends(['aaa', 'be', 'abc', 'hello'])
-    1
     """
-    raise NotImplementedError
+    count = 0
+    for e in s:
+        if len(e) > 1:
+            if e[0] == e[-1]:
+                count += 1
+    return count
+
+print(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']))
+print(match_ends(['', 'x', 'xy', 'xyx', 'xx']))
+print(match_ends(['aaa', 'be', 'abc', 'hello']))
 
 
 def front_x(words):
@@ -25,14 +27,15 @@ def front_x(words):
     e.g. ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] yields
          ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'].
 
-    >>> front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa'])
     ['xaa', 'xzz', 'axx', 'bbb', 'ccc']
-    >>> front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa'])
     ['xaa', 'xcc', 'aaa', 'bbb', 'ccc']
-    >>> front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark'])
     ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
     """
-    raise NotImplementedError
+    return sorted(words, key=lambda x: ('z' + x) if not x[0] == 'x' else x)
+
+print(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']))
+print(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']))
+print(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']))
 
 
 def sort_last(tuples):
@@ -49,7 +52,13 @@ def sort_last(tuples):
     >>> sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)])
     [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
     """
-    raise NotImplementedError
+    return sorted(tuples, key=lambda x: x[-1])
+
+print(sort_last([(1, 3), (3, 2), (2, 1)]))
+print(sort_last([(2, 3), (1, 2), (3, 1)]))
+print(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]))
+
+
 
 
 def remove_adjacent(nums):
@@ -68,7 +77,19 @@ def remove_adjacent(nums):
     >>> remove_adjacent([])
     []
     """
-    raise NotImplementedError
+    for n in reversed(range(len(nums))):
+        if n == 0:
+            break
+        else:
+            if nums[n] == nums[n-1]:
+                nums.pop(n)
+    return nums
+
+print(remove_adjacent([1, 2, 2, 3]))
+print(remove_adjacent([2, 2, 3, 3, 3]))
+print(remove_adjacent([3, 2, 3, 3, 3]))
+print(remove_adjacent([]))
+
 
 
 def linear_merge(list1, list2):
@@ -85,4 +106,14 @@ def linear_merge(list1, list2):
     >>> linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb'])
     ['aa', 'aa', 'aa', 'bb', 'bb']
     """
-    raise NotImplementedError
+    l = []
+    while list1 and list2:
+        if list1[0] < list2[0]:
+            l.append(list1.pop(0))
+        else:
+            l.append(list2.pop(0))
+    return l + list1 + list2
+
+print(linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc']))
+print(linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz']))
+print(linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb']))
